58b9ad133dfef8168d09e9237e8070f8d225ea1f
Guilherme Campbell <guilhermegilcampbell@gmail.com>
2021-10-13 18:10:48 -0300

    Classes

---
+++ POO/src/class/anatomia.js
@@ -0,0 +1,20 @@
+class Person{
+    constructor(name, occupation, age){
+        this.name = name
+        this.occupation = occupation
+        this.age = age
+    }
+
+    hello(name,occupation,age){
+        if (occupation === "dev"){
+            return `Fala ${occupation} seu nome é ${name}`
+        } else {
+            return `Fala ${name} e você tem ${age} anos e sua ocupação é ${occupation}`
+        }
+
+    }
+}
+
+const Pessoa = new Person;
+
+module.exports = Pessoa;
--- POO/src/index.js
+++ POO/src/index.js
@@ -1,7 +1,10 @@
 const ObjJavascript = require('./literals/objeto')
 const ObjJSON = require('./literals/objeto.json')
+const Pessoa = require('./class/anatomia')
 
-console.table(ObjJavascript);
-console.log(ObjJavascript.getFinalPrice());
+// console.table(ObjJavascript);
+// console.log(ObjJavascript.getFinalPrice());
 
-console.log(ObjJSON.produto[1].name);
+// console.log(ObjJSON.produto[1].name);
+
+console.log(Pessoa.hello('Douglas','dev',33))
