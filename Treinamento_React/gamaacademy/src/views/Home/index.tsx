70d371d580ef372cfd353a8729b9111556565d0f
Guilherme Campbell <guilhermegilcampbell@gmail.com>
2021-10-25 18:33:13 -0300

    Inicializaçao do react

---
+++ Treinamento_React/gamaacademy/src/views/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const Home: React.FC = () => {
+  return (
+      <Container>
+          <h1>Home</h1>
+      </Container>
+  );
+}
+
+export default Home;
