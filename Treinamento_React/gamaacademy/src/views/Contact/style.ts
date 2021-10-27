e0633ecce14940084d4c50ce843af0b788e2fd91
Guilherme Campbell <guilhermegilcampbell@gmail.com>
2021-10-27 19:17:17 -0300

    atualizacao

--- Treinamento_React/gamaacademy/src/views/Contact/style.ts
+++ Treinamento_React/gamaacademy/src/views/Contact/style.ts
@@ -7,10 +7,21 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     background-color: green;
+    color: #fff;
     .form-wrapper{
         display:grid;
+        text-align: center;
         .card{
             background-color: #fff;
+            padding: 22px;
+            border-radius: 22px;
+
+            form{
+                display: grid;
+                input{
+                    margin: 10px 0 0;
+                }
+            }
         }
     }
 
