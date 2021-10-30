f83b307a4945f654ad1fbccb5bc621619d08dec1
Guilherme Campbell <guilhermegilcampbell@gmail.com>
2021-10-30 19:46:15 -0300

    API conflicts fixed

---
+++ Treinamento_React/gamaacademy/src/services/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: 'https://webhook.site/eb7a309c-05f9-44aa-a432-72c452fbe201'
+})
--- Treinamento_React/gamaacademy/src/views/Contact/index.tsx
+++ Treinamento_React/gamaacademy/src/views/Contact/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom"
+import React, { useState, useCallback, FormEvent } from 'react';
+import { Link } from "react-router-dom";
 import { Container } from './style';
+import { api } from '../../services/api';
 
 
 interface IData {
@@ -8,22 +9,55 @@ interface IData {
     email: string;
 }
 
+const teste:string = "teste";
+
 const Contact: React.FC = () => {
+
     const [data, setData] = useState<IData>({} as IData);
+    const [submit, setSubmit] = useState(false);
+
+    const handleSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        api.post('',JSON.stringify(data))
+        .then(response => {
+            console.log(response);
+            if (response.status === 200) {
+                setSubmit(true);
+            }
+        })
+    }, [data])
+
+
     return (
         <Container>
             <div className="form-wrapper" >
                 <h1>Contact</h1>
                 Name: {data?.name}
-                <br/>
+                <br />
                 Email: {data?.email}
 
                 <div className="card">
-                    <form onSubmit={()=>{}}>
-                        <input type="text" placeholder="nome" onChange={e=>setData({...data, name : e.target.value})}/>
-                        <input type="text" placeholder="nome"  onChange={e=>setData({...data, email : e.target.value})}/>
-                        <input type="Submit" value="cadastrar" />
-                    </form>
+                    {submit ? (
+                        <div>
+                            <h1>Obrigado pelo envio dos datas</h1>
+                        </div>
+                    ) : (
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="nome"
+                                onChange={e => setData({ ...data, name: e.target.value })}
+                            />
+                            <input
+                                type="email"
+                                placeholder="email"
+                                onChange={e => setData({ ...data, email: e.target.value })}
+                            />
+                            <input type="submit" value="cadastrar" />
+                        </form>
+                    )
+                    }
+
                 </div>
                 <Link to="/">
                     Retornar à página principal
